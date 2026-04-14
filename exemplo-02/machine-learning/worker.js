01199bf5318f40fda4e230ffced0570ef1764a18
Andres Payema <andres@packiyo.com>
2026-04-14 07:41:05 -0300

    finish product

--- exemplo-02/machine-learning/worker.js
+++ exemplo-02/machine-learning/worker.js
@@ -3,53 +3,40 @@ importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
 const MODEL_PATH = `yolov5n_web_model/model.json`;
 const LABELS_PATH = `yolov5n_web_model/labels.json`;
 const INPUT_MODEL_DIMENTIONS = 640
+const CLASSES_THRESHOLD = 0.4
 
 let _labels = []
 let _model = null
-async function loadModelAndLabels() {
-    await tf.ready()
 
+async function loadModelAndLabels(){
+    await tf.ready()
     _labels = await (await fetch(LABELS_PATH)).json()
     _model = await tf.loadGraphModel(MODEL_PATH)
-
-    // warmup
+    
+    //warmup
     const dummyInput = tf.ones(_model.inputs[0].shape)
     await _model.executeAsync(dummyInput)
     tf.dispose(dummyInput)
-
-    postMessage({ type: 'model-loaded' })
-
+    postMessage({type: 'model_loaded'})
 }
 
-/**
- * Pré-processa a imagem para o formato aceito pelo YOLO:
- * - tf.browser.fromPixels(): converte ImageBitmap/ImageData para tensor [H, W, 3]
- * - tf.image.resizeBilinear(): redimensiona para [INPUT_DIM, INPUT_DIM]
- * - .div(255): normaliza os valores para [0, 1]
- * - .expandDims(0): adiciona dimensão batch [1, H, W, 3]
- *
- * Uso de tf.tidy():
- * - Garante que tensores temporários serão descartados automaticamente,
- *   evitando vazamento de memória.
- */
-function preprocessImage(input) {
-    return tf.tidy(() => {
+function preprocessImage(input){
+    return tf.tidy(()=>{
+        // converte imageBitmap para tensor
         const image = tf.browser.fromPixels(input)
 
         return tf.image
-            .resizeBilinear(image, [INPUT_MODEL_DIMENTIONS, INPUT_MODEL_DIMENTIONS])
-            .div(255)
-            .expandDims(0)
+        .resizeBilinear(image, [INPUT_MODEL_DIMENTIONS, INPUT_MODEL_DIMENTIONS]) //redimensiona para array
+        .div(255)   //normaliza os valores para 0,1
+        .expandDims(0)  //adiciona dimensao batch
     })
 }
 
-async function runInference(tensor) {
-    const output = await _model.executeAsync(tensor)
+async function runInference(tensor){
+    const output =  await _model.executeAsync(tensor)
     tf.dispose(tensor)
-    // Assume que as 3 primeiras saídas são:
-    // caixas (boxes), pontuações (scores) e classes
 
-    const [boxes, scores, classes] = output.slice(0, 3)
+    const [boxes,scores,classes] = output.slice(0,3)
     const [boxesData, scoresData, classesData] = await Promise.all(
         [
             boxes.data(),
@@ -59,32 +46,57 @@ async function runInference(tensor) {
     )
 
     output.forEach(t => t.dispose())
-
+    
     return {
         boxes: boxesData,
         scores: scoresData,
-        classes: classesData
+        classes: classesData,
     }
 }
-loadModelAndLabels()
+
+function * processPrediction ({ boxes, scores, classes}, width, height){
+    for(let index =0; index<scores.length; index++){
+
+        if(scores[index] < CLASSES_THRESHOLD) continue
+        
+        const label = _labels[classes[index]]
+        if(label !== 'kite') continue
+
+        let [x1,y1,x2,y2] = boxes.slice(index*4, (index +1) * 4)
+        x1 *= width
+        x2 *= width
+        y1 *= height
+        y2 *= height
+
+        const boxWidth = x2-x1
+        const boxHeight = y2-y1
+        const centerX = x1 + boxWidth/2
+        const centerY = y1 + boxHeight/2
+
+        yield {
+            x:centerX,
+            y: centerY,
+            score: (scores[index]* 100).toFixed(2)
+        }
+    }
+}
+
+loadModelAndLabels();
 
 self.onmessage = async ({ data }) => {
     if (data.type !== 'predict') return
-    if (!_model) return
-
+    if(!_model) return
     const input = preprocessImage(data.image)
-    const { width, height } = data.image
+    const {width, height} = data.image
 
     const inferenceResults = await runInference(input)
 
-    debugger
-    postMessage({
-        type: 'prediction',
-        x: 400,
-        y: 400,
-        score: 0
-    });
-
+    for( const prediction of processPrediction(inferenceResults, width, height)){
+        postMessage({
+            type: 'prediction',
+            ...prediction
+        });
+    }
 
 };
 
